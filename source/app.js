1ca96dc118ad7114657ba04afeb8ab28b7eccfff
Franco Menicucci <menicuccifranco23@gmail.com>
2024-02-28 17:29:13 -0300

    Segundo Commit: Cambios

--- source/app.js
+++ source/app.js
@@ -1,39 +1,43 @@
 // App.js
 
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import Handlebars from 'handlebars';
-import viewsRouter from './routes/views.router.js';
-import { Server } from 'socket.io';
+const express = require('express');
+const http = require('http');
+const { fileURLToPath } = require('url');
+const { dirname } = require('path');
+const Handlebars = require('handlebars');
+const viewsRouter = require('./routes/views.router.js');
+const { Server } = require('socket.io');
 
 const app = express();
-const httpServer = app.listen(8080, () => console.log("Listening on PORT 8080"));
+const httpServer = http.createServer(app);
 const socketServer = new Server(httpServer);
+const PORT = 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+let products = []; // Inicializar la variable 'products'
+
 app.engine('handlebars', Handlebars.engine());
 app.set('views', __dirname + '/views');
-app.set('views engine', 'handlebars');
+app.set('view engine', 'handlebars'); // PIENSO QUE ACA ESTABA EL ERROR EN CUANTO AL VIEWS
 app.use(express.static(__dirname + '/public'));
 app.use('/', viewsRouter);
 
-
-
 socketServer.on('connection', socket => {
     console.log("Nuevo Cliente conectado");
-    
 
-socket.on('addProduct', (productName) => {
-    products.push(productName);
-    socketServer.emit('products', products);
+    socket.on('addProduct', (productName) => {
+        products.push(productName);
+        socketServer.emit('products', products);
+    });
+
+    socket.on('deleteProduct', (productName) => {
+        products = products.filter(product => product !== productName);
+        socketServer.emit('products', products);
+    });
 });
 
-socket.on('deleteProduct', (productName) => {
-    products = products.filter(product => product !== productName);
-    socketServer.emit('products', products);
-});
-
+httpServer.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
 });
